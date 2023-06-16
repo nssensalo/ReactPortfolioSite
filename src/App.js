22041d83b9188e34b22fa698b7410bffb85b1446
Nansamba Ssensalo <nssensalo@gmail.com>
2023-06-15 19:49:55 -0700

    rearranging file structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+function App() {
+  return (
+    <div>
+        <h1  className="heading">Nansamba's Portfolio</h1>
+        <img className="circle-img" src="https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_1280.jpg" alt="image" />
+      
+      <body>
+        <table cellspacing="20">
+              <td><img src="images/nanpngformat.png" width="250" height="250" alt="Nansamba Ssensalo photo"/></td>
+              <td><h1>
+                  Nansamba Ssensalo
+              </h1>
+              <p><em><strong>Web Developer - Software Engineer - FinTech</strong></em></p></td>
+          </table>
+      </body>
+    </div>
+  );
+}
+
+export default App;
